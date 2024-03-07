6138c40813f785d9b6e37b159a4c8e6e784578e1
reynaldomarchell <reynaldomarchellba@gmail.com>
2024-03-07 23:38:23 +0700

    feat: add GitHub and LinkedIn link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Github } from "lucide-react";
+import { Linkedin } from "lucide-react";
 import Activities from "@/components/Activities";
 
 export default function Home() {
   return (
     <div className="flex flex-col h-dvh md:h-full justify-start text-slate-200 bg-slate-950 font-mono py-6 px-4">
-      <div className="py-4">
+      <div className="py-4 space-y-2">
         <a href="https://git.io/typing-svg">
           <img
             src="https://readme-typing-svg.demolab.com?font=Fira+Code&duration=3000&pause=100&color=F8FAFC&vCenter=true&random=true&width=435&lines=Hello;%E3%81%93%E3%82%93%E3%81%AB%E3%81%A1%E3%81%AF;%EC%95%88%EB%85%95%ED%95%98%EC%84%B8%EC%9A%94;%E6%82%A8%E5%A5%BD;Bonjour;%E0%A4%A8%E0%A4%AE%E0%A4%B8%E0%A5%8D%E0%A4%A4%E0%A5%87;%CE%A7%CE%B1%CE%AF%CF%81%CE%B5%CF%84%CE%B5;%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5;Ol%C3%A1;%D9%85%D8%B1%D8%AD%D8%A8%D8%A7%D9%8B"
@@ -14,6 +16,14 @@ export default function Home() {
           Reynaldo Marchell Bagas Adji
         </h1>
         <p>A person who loves to code and learn new things 💖</p>
+        <div className="flex gap-2">
+          <a href="https://github.com/reynaldomarchell">
+            <Github />
+          </a>
+          <a href="https://www.linkedin.com/in/reynaldo-marchell/">
+            <Linkedin />
+          </a>
+        </div>
       </div>
       <div>
         <Activities />
