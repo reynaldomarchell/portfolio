d50f6a1bb3bd5adbc47fcfa281c5778b0de6b214
reynaldomarchell <reynaldomarchellba@gmail.com>
2024-03-09 10:40:40 +0700

    refactor: move some code to a new components

---
+++ src/components/Spotify.tsx
@@ -0,0 +1,11 @@
+import Markdown from "react-markdown";
+
+export default function Spotify() {
+  return (
+    <div className="flex justify-center">
+      <Markdown>
+        [![spotify-github-profile](https://spotify-github-profile.vercel.app/api/view?uid=31ia3zscq2wmtywnd3kdbfkb55qq&cover_image=true&theme=default&show_offline=true&background_color=0f172a&interchange=false&bar_color_cover=true)](https://spotify-github-profile.vercel.app/api/view?uid=31ia3zscq2wmtywnd3kdbfkb55qq&redirect=true)
+      </Markdown>
+    </div>
+  );
+}
