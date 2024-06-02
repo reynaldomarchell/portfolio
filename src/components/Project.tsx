aed5b8ec9bf059cc9407de86f77e7ff90b6e4353
reynaldomarchell <reynaldomarchellba@gmail.com>
2024-06-03 00:02:35 +0700

    fix: image

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -32,7 +32,7 @@ const projectData: projectType[] = [
     title: "YumeNime",
     description:
       "🌙 YumeNime is an ad-free and responsive anime streaming website created using Next.js",
-    image: "/yumenime1.png",
+    image: "/yumenime.png",
     link: "https://yumenime.vercel.app",
     tools: (
       <>
@@ -46,7 +46,7 @@ const projectData: projectType[] = [
   {
     title: "Chat Aoba",
     description: "💌 Aoba is a 2D AI assistant powered by a Gemini model",
-    image: "/aoba1.png",
+    image: "/aoba.png",
     link: "https://chat-aoba.vercel.app",
     tools: (
       <>
@@ -62,7 +62,7 @@ const projectData: projectType[] = [
     title: "BIMSQ Sort Visualizer",
     description:
       "BIMSQ is an abbreviation for Bubble, Insertion, Merge, Selection, and Quick Sort. This app will display a visualization of the five types of sorting above.",
-    image: "/bimsq1.png",
+    image: "/bimsq.png",
     link: "https://bimsq-sort.vercel.app",
     tools: (
       <>
@@ -76,7 +76,7 @@ const projectData: projectType[] = [
     title: "Bantur",
     description:
       "Introducing Bandung City Tour—a meticulously crafted itinerary designed to showcase the city highlights.",
-    image: "/bantur1.png",
+    image: "/bantur.png",
     link: "https://bantur.vercel.app",
     tools: (
       <>
@@ -90,7 +90,7 @@ const projectData: projectType[] = [
     title: "Restoranku",
     description:
       "Restoranku is a powerful dashboard page designed to assist restaurant owners in efficiently monitoring and managing their operations.",
-    image: "/restoranku1.png",
+    image: "/restoranku.png",
     link: "https://github.com/reynaldomarchell/restoranku.git",
     tools: (
       <>
@@ -108,7 +108,7 @@ const projectData: projectType[] = [
   {
     title: "Movie List",
     description: "Create a list for your favorite movies.",
-    image: "/movie-list1.png",
+    image: "/movie-list.png",
     link: "https://github.com/reynaldomarchell/movie-list",
     tools: (
       <>
@@ -121,7 +121,7 @@ const projectData: projectType[] = [
     title: "Apparance",
     description:
       'E-commerce project "Apparance" is a online fashion shop for selling or buying clothes, hoodie, sneakers, etc.',
-    image: "/apparance1.png",
+    image: "/apparance.png",
     link: "https://github.com/reynaldomarchell/FinalProject-BackendDevelopment-2022.git",
     tools: (
       <>
@@ -136,7 +136,7 @@ const projectData: projectType[] = [
     title: "To-do",
     description:
       "To-do list web page using PHP and MySQL as a database for storing user account and each user task.",
-    image: "/todo3.png",
+    image: "/todo.png",
     link: "https://github.com/reynaldomarchell/MidProject-BackendDevelopment-2022.git",
     tools: (
       <>
