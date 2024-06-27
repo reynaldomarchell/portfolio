fd02fa1ffdf60860d4b166c6d970efda0ee1276e
reynaldomarchell <reynaldomarchellba@gmail.com>
2024-06-27 10:40:19 +0700

    fix: spotify endpoint

--- src/components/Spotify.tsx
+++ src/components/Spotify.tsx
@@ -4,7 +4,7 @@ export default function Spotify() {
   return (
     <div className="flex justify-center">
       <Markdown>
-        [![spotify-github-profile](https://spotify-github-profile.vercel.app/api/view?uid=31ia3zscq2wmtywnd3kdbfkb55qq&cover_image=true&theme=default&show_offline=true&background_color=0f172a&interchange=true&bar_color_cover=true)](https://spotify-github-profile.vercel.app/api/view?uid=31ia3zscq2wmtywnd3kdbfkb55qq&redirect=true)
+        [![spotify-github-profile](https://spotify-github-profile.kittinanx.com/api/view?uid=31ia3zscq2wmtywnd3kdbfkb55qq&cover_image=true&theme=default&show_offline=true&background_color=0f172a&interchange=true&bar_color_cover=true)](https://spotify-github-profile.vercel.app/api/view?uid=31ia3zscq2wmtywnd3kdbfkb55qq&redirect=true)
       </Markdown>
     </div>
   );
