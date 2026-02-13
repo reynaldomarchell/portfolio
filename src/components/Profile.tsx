9ddf026f7ddbb046ec6dfacf45cd65053831cbe6
reynaldomarchell <reynaldomarchellba@gmail.com>
2026-02-14 05:39:42 +0700

    fix: update CV download link in Profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -16,7 +16,7 @@ export default function Profile() {
       <p>A person who loves to code and learn new things 💖</p>
 
       <Link
-        href="https://drive.google.com/file/d/1M1Qq4pmlmmS6rknUSg0RY6mWLNYpzC82/view?usp=sharing"
+        href="https://drive.google.com/file/d/1GkgRCeVehODemBYqYC3hE_g9C5dBbCDQ/view?usp=drive_link"
         className="justify-center flex border rounded-md py-2 px-4 hover:bg-slate-900 hover:transition-all hover:duration-500 hover:cursor-pointer hover:text-slate-50 gap-2 items-center"
         target="_blank"
         rel="noreferrer"
